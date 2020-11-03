68adad26c3f94f380a0e85c11fa9df251dc08860
kideveloper612 <51858897+kideveloper612@users.noreply.github.com>
2020-11-03 22:52:50 +0800

    add header

--- mail.py
+++ mail.py
@@ -18,27 +18,46 @@ def write(lines):
         writer.writerows(lines)
 
 
-imap = imaplib.IMAP4_SSL("imap.gmail.com")
-imap.login(username, password)
+def main():
+    global From
+    if not os.path.isfile(save_name):
+        header = [['Subject', 'From', 'Date', 'Name', 'Phone', 'DOB', 'Office', 'Note']]
+        write(header)
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    imap.login(username, password)
 
-imap.select("INBOX")
+    imap.select("INBOX")
 
-sub_status, messages = imap.search(None, 'FROM {}'.format(From))
-if sub_status == 'OK':
-    for message in messages[0].split():
-        res, msg = imap.fetch(message, "(RFC822)")
-        for response in msg:
-            if isinstance(response, tuple):
-                msg = email.message_from_bytes(response[1])
-                subject = decode_header(msg["Subject"])[0][0]
-                if isinstance(subject, bytes):
-                    subject = subject.decode()
-                From, encoding = decode_header(msg.get("From"))[0]
-                if isinstance(From, bytes):
-                    From = From.decode(encoding)
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        payload = part.get_payload(decode=True)
+    sub_status, messages = imap.search(None, 'FROM {}'.format(From))
+    if sub_status == 'OK':
+        for message in messages[0].split():
+            res, msg = imap.fetch(message, "(RFC822)")
+            for response in msg:
+                if isinstance(response, tuple):
+                    msg = email.message_from_bytes(response[1])
+                    subject = decode_header(msg["Subject"])[0][0]
+                    if isinstance(subject, bytes):
+                        subject = subject.decode()
+                    From, encoding = decode_header(msg.get("From"))[0]
+                    if isinstance(From, bytes):
+                        From = From.decode(encoding)
+                    if msg.is_multipart():
+                        for part in msg.walk():
+                            payload = part.get_payload(decode=True)
+                            if payload is not None:
+                                text = payload.decode('utf-8').replace('\r', '').replace('\n', ' ')
+                                date = re.search('=================(.*)======================================', text).group(1).strip()
+                                name = re.search('Name:(.*)Ph#:', text).group(1).strip()
+                                phone = re.search('Ph#:(.*)DOB:', text).group(1).strip()
+                                birthday = re.search('DOB:(.*)Which office were you seen at', text).group(1).strip()
+                                office = re.search('Which office were you seen at(.*)Msg:', text).group(1)[2:].strip()
+                                note = re.search('Msg(.*)CID:', text).group(1)[1:].strip()
+                                line = [subject, msg['from'], date, name, phone, birthday, office, note]
+                                print(line)
+                                write([line])
+                                break
+                    else:
+                        payload = msg.get_payload(decode=True)
                         if payload is not None:
                             text = payload.decode('utf-8').replace('\r', '').replace('\n', ' ')
                             date = re.search('=================(.*)======================================', text).group(1)
@@ -50,20 +69,11 @@ if sub_status == 'OK':
                             line = [subject, msg['from'], date, name, phone, birthday, office, note]
                             print(line)
                             write([line])
-                            break
-                else:
-                    payload = msg.get_payload(decode=True)
-                    if payload is not None:
-                        text = payload.decode('utf-8').replace('\r', '').replace('\n', ' ')
-                        date = re.search('=================(.*)======================================', text).group(1)
-                        name = re.search('Name:(.*)Ph#:', text).group(1).strip()
-                        phone = re.search('Ph#:(.*)DOB:', text).group(1).strip()
-                        birthday = re.search('DOB:(.*)Which office were you seen at', text).group(1).strip()
-                        office = re.search('Which office were you seen at(.*)Msg:', text).group(1)[2:].strip()
-                        note = re.search('Msg(.*)CID:', text).group(1)[1:].strip()
-                        line = [subject, msg['from'], date, name, phone, birthday, office, note]
-                        print(line)
-                        write([line])
 
-imap.close()
-imap.logout()
+    imap.close()
+    imap.logout()
+
+
+if __name__ == '__main__':
+    save_name = 'result.csv'
+    main()
